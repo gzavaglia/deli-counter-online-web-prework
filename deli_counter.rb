14260a45d67c596f39de404aeba143e710a1e770
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-04-11 23:44:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,12 @@
 require 'pry'
 
+# we've removed the parameters from the methods 
+# We will now be using only numbers -- no names 
+# The deli line should now start at [210, 211]
+# The current number hanging on the wheel is 212
+
 katz_deli = []
-def line(katz_deli)
+def line
   line_size = katz_deli.size 
   if line_size==0 
   puts "The line is currently empty."
@@ -13,14 +18,14 @@ def line(katz_deli)
   end  #end if 
 end  #end line()
 
-def take_a_number(katz_deli, name)
+def take_a_number
   katz_deli << name
   len = katz_deli.size
   #katz_deli.each_with_index {|val,index| puts "Welcome #{name}. You are number #{index}"
   puts "Welcome, #{name}. You are number #{len} in line."
 end #end take_a_number()
 
-def now_serving(katz_deli)
+def now_serving
   len = katz_deli.size 
   if len == 0 
     puts "There is nobody waiting to be served!"
